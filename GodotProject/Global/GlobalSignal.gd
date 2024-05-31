ad48ca00fcb842a67b03fc0c60b90183c3dc7ded
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2024-05-31 17:59:44 +0900

    1
    1

--- GodotProject/Global/GlobalSignal.gd
+++ GodotProject/Global/GlobalSignal.gd
@@ -10,9 +10,15 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
+	if OS.has_feature('web'):
+		if JavaScriptBridge.get_interface("is_test_pressed") != null:
+			monitoring_test_btn()
+
+func monitoring_test_btn() -> void:
 	if JavaScriptBridge.get_interface("is_test_pressed").valueOf() != test_btn:
 		emit_count += 1
 		press_button.emit()
 		window.set_count_label(
 			JavaScriptBridge.create_object("count", emit_count)
 			)
+	
