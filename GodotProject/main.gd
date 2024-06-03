59f3b37824105620341e2f60e75b57f9905d9f44
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2024-06-04 02:28:33 +0900

    1
    1

--- GodotProject/main.gd
+++ GodotProject/main.gd
@@ -20,23 +20,22 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	if OS.has_feature('web'):
-
 		get_values(a, "a")
 		get_values(b, "b")
 		get_values(c, "c")
 		get_values(d, "d")
 
-		t_scale = JavaScriptBridge.get_interface("t_scale").valueOf()
-
+		get_time_scale()
+		
 		window.set_values()
 	
-	color_rect.material.set("shader_parameter/a_index", a)
-	color_rect.material.set("shader_parameter/b_index", b)
-	color_rect.material.set("shader_parameter/c_index", c)
-	color_rect.material.set("shader_parameter/d_index", d)
-	color_rect.material.set("shader_parameter/time_scale", t_scale)
 
 func get_values(color_vec3:Vector3, index:String) -> void:
 	color_vec3.x = JavaScriptBridge.get_interface(index + "_r").valueOf()
 	color_vec3.y = JavaScriptBridge.get_interface(index + "_g").valueOf()
 	color_vec3.z = JavaScriptBridge.get_interface(index + "_b").valueOf()
+	color_rect.material.set("shader_parameter/" + index +"_index", color_vec3)
+
+func get_time_scale() -> void:
+	t_scale = JavaScriptBridge.get_interface("t_scale").valueOf()
+	color_rect.material.set("shader_parameter/time_scale", t_scale)
