1192bd7d8c275b481e219cf34edcacec85507bde
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2024-06-03 12:55:31 +0900

    1
    1

--- JsTemplete/navtemp.js
+++ JsTemplete/navtemp.js
@@ -1,86 +1,61 @@
-const top_table = document.getElementById("top-table");
-/*
-<div class="container-fluid">
-    <a href="#" class="navbar-brand">Godot-Web</a>
-    <div class="btn-group dropstart" role="group">
-        <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Docs</button>
-        <ul class="dropdown-menu " id="ddmenu">
-            <li><a class="dropdown-item" href="/installation/index.html">Install Godot</a></li>
-            <li><a class="dropdown-item" href="/setEnv/index.html">Set Environment</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="/godotToJs/index.html">GodotToWeb</a></li>
-            <li><a class="dropdown-item" href="#">Example - 1</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="/webToGodot/index.html">WebToGodot</a></li>
-            <li><a class="dropdown-item" href="#">Example - 2</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="/Debug/index.html">Debugging</a></li>
-            <li><a class="dropdown-item" href="/Debug/index.html">AutoLoad</a></li>
-            <li><a class="dropdown-item" href="/Debug/index.html">Using Signal</a></li>
-        </ul>
-    </div>
-</div>
-*/
+const btnGroup = document.getElementById("btn-group")
+const ddMenu = document.createElement("ul")
 
-set_top_table();
+set_nav_table();
 
-function set_top_table() {
-   
-    var ddMenu = document.createElement("ul")
+function set_nav_table() {
+
     ddMenu.className = "dropdown-menu"
     ddMenu.id = "ddmenu"
-
-    btnGroup.append(ddMenu)
-
-    var dditemStr = "dropdown-item"
-    var dividerStr = "dropdown-divider"
-
-    var li_1 = document.createElement("li")
-    var a_1 = document.createElement("a")
-    li_1.append(a_1)
-    a_1.className = "dropdown-item"
-    a_1.href = "/installation/index.html"
-    a_1.innerHTML = "Install Godot"
-    ddMenu.append(li_1)
-
-    var li_2 = document.createElement("li")
-    var a_2 = document.createElement("a")
-    create_li(li_2, a_2, dditemStr, "/setEnv/index.html",'Set Environment')
-    ddMenu.append(li_2)
     
+    btnGroup.append(ddMenu)
+    
+    var dditemStr = "dropdown-item"
+    
+    create_li("dropdown-item", "/installation/index.html", "Install Godot")
+    create_li("dropdown-item", "/setEnv/index.html", "Set Environment")
+    create_divider()
+    create_li("dropdown-item", "/godotToWeb/index.html", "GodotToWeb")
+    create_li("dropdown-item", "#", "Example - 1")
+    create_divider()
+    create_li("dropdown-item", "/webToGodot/index.html", "WebToGodot")
+    create_li("dropdown-item", "#", "Example - 2")
+    create_divider()
+    create_li("dropdown-item", "#", "Debugging")
+    create_li("dropdown-item", "#", "Using GlobalSignal")
+    create_li("dropdown-item", "#", "Set Environment")
+}
+
+function create_li(c_name, ref, listName) {
+    var li = document.createElement("li")
+    var a = document.createElement("a")
+    li.append(a)
+    a.className = c_name
+    a.href = ref
+    a.innerHTML = listName
+    ddMenu.append(li)
+}
+function create_divider() {
     var divide_li = document.createElement("li")
     var hr = document.createElement("hr")
     hr.className = "dropdown-divider"
     divide_li.append(hr)
     ddMenu.append(divide_li)
-
-    var li_3 = document.createElement("li")
-    var a_3 = document.createElement('a')
-    create_li(li_3, a_2, dditemStr, "/godotToWeb/index.html", 'GodotToWeb')
-
-    var li_4 = document.createElement("li")
-    var a_4 = document.createElement('a')
-
-    var li_5 = document.createElement("li")
-    var a_5 = document.createElement('a')
-
-    var li_6 = document.createElement("li")
-    var a_6 = document.createElement('a')
-
-    var li_7 = document.createElement("li")
-    var a_7 = document.createElement('a')
-
-    var li_8 = document.createElement("li")
-    var a_8 = document.createElement('a')
-
-    var li_9 = document.createElement("li")
-    var a_9 = document.createElement('a')
-
 }
 
-function create_li(li, a, className, ref, listName){
-    li.append(a)
-    a.className = className
-    a.href = ref
-    a.innerHTML = listName
-}
+/* !!!!Preview HTML Code(미리보기)!!!!
+                <ul class="dropdown-menu " id="ddmenu">
+                    <li><a class="dropdown-item" href="/installation/index.html">Install Godot</a></li>
+                    <li><a class="dropdown-item" href="/setEnv/index.html">Set Environment</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="/godotToJs/index.html">GodotToWeb</a></li>
+                    <li><a class="dropdown-item" href="#">Example - 1</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="/webToGodot/index.html">WebToGodot</a></li>
+                    <li><a class="dropdown-item" href="#">Example - 2</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="/Debug/index.html">Debugging</a></li>
+                    <li><a class="dropdown-item" href="/Debug/index.html">AutoLoad</a></li>
+                    <li><a class="dropdown-item" href="/Debug/index.html">Using Signal</a></li>
+                </ul>
+*/
