96213e658a58ef41c56cef445856e99d1d398ab5
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2024-06-07 04:50:55 +0900

    1
    1

--- GodotProject/Global/GlobalSignal.gd
+++ GodotProject/Global/GlobalSignal.gd
@@ -1,8 +1,8 @@
 extends Node
 
 signal button_on_click(count:int)
-signal paste_on_click(data)
-signal clear_btn_pressed
+signal paste(count:int, data:Image)
+signal clear
 
 var window:JavaScriptObject = JavaScriptBridge.get_interface("window")
 var document:JavaScriptObject = JavaScriptBridge.get_interface("document")
@@ -11,7 +11,6 @@ var add_label_btn:bool = false
 var emit_count:int = 0
 
 var paste_btn:bool = false
-
 var clear_btn:bool = false
 
 func _ready() -> void:
@@ -21,18 +20,8 @@ func _process(delta: float) -> void:
 	if OS.has_feature('web'):
 		if document.title == "GlobalSignal":
 			monitoring_test_btn()
-		if document.title == "testClipBoard":
 			monitoring_paste()
-
-func monitoring_paste() -> void:
-	var _btn:bool = JavaScriptBridge.get_interface("is_paste_pressed").valueOf()
-	if _btn != paste_btn:
-		paste_btn = _btn
-		paste_on_click.emit()
-		if DisplayServer.clipboard_has_image():
-			var data:Image = DisplayServer.clipboard_get_image()
-			paste_on_click.emit(data)
-		window.complete_paste()
+			monitoring_clear_btn()
 
 func monitoring_test_btn() -> void:
 	var _btn:bool = JavaScriptBridge.get_interface("is_test_pressed").valueOf()
@@ -40,12 +29,18 @@ func monitoring_test_btn() -> void:
 		add_label_btn = _btn
 		emit_count += 1
 		button_on_click.emit(emit_count)
-		window.set_count_label(
-			JavaScriptBridge.create_object("Number", emit_count)
-			)
+
+func monitoring_paste() -> void:
+	var _btn:bool = JavaScriptBridge.get_interface("is_paste_pressed").valueOf()
+	if _btn != paste_btn:
+		paste_btn = _btn
+		emit_count += 1
+		if DisplayServer.clipboard_has_image():
+			var data:Image = DisplayServer.clipboard_get_image()
+			paste.emit(emit_count, data)
 
 func monitoring_clear_btn() -> void:
 	var btn:bool = JavaScriptBridge.get_interface("is_clear_pressed").valueOf()
 	if btn != clear_btn:
 		clear_btn = btn
-		clear_btn_pressed.emit()
+		clear.emit()
--- GodotProject/test_signal.gd
+++ GodotProject/test_signal.gd
@@ -5,23 +5,27 @@ extends Control
 
 func _ready()->void:
 	GlobalSignal.press_button.connect(on_press_test_button)
-	GlobalSignal.paste_on_click.connect(on_paste_button_click)
+	GlobalSignal.paste.connect(on_paste_button_click)
+	GlobalSignal.clear.connect(clear)
 	
 func on_press_test_button(count:int) -> void:
-	create_label(count)
-
-func on_paste_button_click(data:Image) -> void:
-	create_img(data)
-
-func create_label(count:int) -> void:
 	var count_label:Label = Label.new()
 	count_label.text = str(count) + "번:"
 	count_panel.add_child(count_label)
 	var result_label:Label = Label.new()
 	result_label.text = "버튼을 눌렀습니다."
 	result_panel.add_child(result_label)
-	
-func create_img(data:Image) -> void:
+
+func on_paste_button_click(count:int, data:Image) -> void:
+	var count_label:Label = Label.new()
+	count_label.text = str(count) + "번에 사진을 붙여넣었습니다:"
+	count_panel.add_child(count_label)
 	var result_texture:TextureRect = TextureRect.new()
 	result_texture.texture = ImageTexture.create_from_image(data)
-	
+	result_panel.add_child(result_texture)
+
+func clear() -> void:
+	for node in count_panel.get_children():
+		node.queue_free()
+	for node in result_panel.get_children():
+		node.queue_free()
